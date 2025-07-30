f7e5d8dca8e14fc57c19a6d0bf5bd5a3830b8ce2
hariprasath <hariprasathh564@gmail.com>
2025-07-30 10:31:51 +0530

    new change

--- pages/index.js
+++ pages/index.js
@@ -22,22 +22,22 @@ for(var i=0; i<noOfButtons; i++){
          function sounds(key){
        switch(key){
     case "s":
-    var audioElement1 = new Audio("/home/hariprashath/Documents/js/assignment2js/assets/sword-33.mp3");
+    var audioElement1 = new Audio("../../assignment2js/assets/sword-33.mp3");
         audioElement1.play();
     break;
 
     case "a":
-    var audioElement2 = new Audio("/home/hariprashath/Documents/js/assignment2js/assets/lion-sms-49434.mp3");
+    var audioElement2 = new Audio("../../assignment2js/assets/lion-sms-49434.mp3");
         audioElement2.play();
     break;
 
     case "d":
-    var audioElement3 = new Audio("/home/hariprashath/Documents/js/assignment2js/assets/000867237-prev-30584 (Copy).mp3");
+    var audioElement3 = new Audio("../../assignment2js/assets/000867237-prev-30584 (Copy).mp3");
         audioElement3.play();
     break;
 
     case "w":
-    var audioElement4 = new Audio("/home/hariprashath/Documents/js/assignment2js/assets/comedy-4722.mp3");
+    var audioElement4 = new Audio("../../assignment2js/assets/comedy-4722.mp3");
         audioElement4.play();
     break;
     default:
